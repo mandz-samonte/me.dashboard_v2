34177f7022be0013c960d87ad247a0cff7d5d733
John Mandy Samonte <johnmandysamonte@gmail.com>
2020-06-06 20:50:40 +0800

    responsive layout

--- client/src/App.js
+++ client/src/App.js
@@ -22,15 +22,15 @@ function App() {
             <Card.Card className="w-full h-20 shadow-md"></Card.Card>
           </div>
 
-          <div className="flex h-full">
-            <div className="w-3/4 mr-5">
-              <Card.Card className="w-full h-full shadow-md">
+          <div className="flex md:flex-col h-full">
+            <div className="w-3/4 md:w-full h-full md:h-2/3 mr-5">
+              <Card.Card className="w-full h-full  shadow-md">
               </Card.Card>
             </div>
 
-            <div className="flex flex-col w-1/4">
+            <div className="flex flex-col md:flex-row md:w-full md:mt-5 md:h-1/3 w-1/4">
               <Card.Card className="w-full h-full shadow-md"></Card.Card>
-              <Card.Card className="w-full h-full shadow-md mt-5"></Card.Card>
+              <Card.Card className="w-full h-full shadow-md mt-5 sm:mt-0 md:mt-0 md:ml-5"></Card.Card>
             </div>
           </div>
         </Content>
--- client/src/layout/Content/index.js
+++ client/src/layout/Content/index.js
@@ -4,7 +4,7 @@ function Content(props) {
   const { children, className } = props;
 
   return (
-    <div className={`${className} p-12 flex flex-col w-11/12 h-screen justify-between`}>
+    <div className={`${className} p-12 flex flex-col w-11/12 sm:w-full h-screen justify-between`}>
       {children}
     </div>
   )
