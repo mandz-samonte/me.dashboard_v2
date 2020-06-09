90e6b3514e0abf8b8e3f66046ebc8fdba0db014f
John Mandy Samonte <johnmandysamonte@gmail.com>
2020-06-09 23:29:33 +0800

    setting up api for habit tracker

---
+++ client/src/api/apiHabitTracker.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const MODULE_PREFIXES = '/api/habit-tracker';
+
+export function index(params) {
+  return axios.get(`${MODULE_PREFIXES}`, params);
+}
+
+export function add(params) {
+  return axios.post(`${MODULE_PREFIXES}`, params);
+}
+
+export function update(id, params) {
+  return axios.put(`${MODULE_PREFIXES}/${id}`, params);
+}
+
+export function delete(id, params) {
+  return axios.delete(`${MODULE_PREFIXES}/${id}`, params);
+}
+
+export default {
+  index,
+  add,
+  update,
+  delete
+}
